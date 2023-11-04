fdc5784c5a583accd8e7c2bae6d839f533c1a8b1
martinsw01 <martin.s.winther@gmail.com>
2023-11-05 00:09:43 +0100

    Enable - mathjax - superfences - admonitoin - content tabs

---
+++ docs/javascripts/mathjax.js
@@ -0,0 +1,37 @@
+window.MathJax = {
+    loader: {
+        load: ['[tex]/html']
+    },
+    tex: {
+        packages: {
+            '[+]': ['html', 'newcommand']
+        },
+        macros: { // custom commands
+            properideal: "{\\ \\mathbin{\\rlap{\\lneq}\\hspace{-0.6ex}\\trianglelefteq}} \\ ",
+            ideal: "{\\ \\trianglelefteq} \\ ",
+            Ker: "{\\operatorname{Ker}}",
+            Im: "{\\operatorname{Im}}",
+            implieshref: ["{\\overset{\\href{#1}{\\text{#2}}}{\\implies}}", 2], // overset \implies with a link
+            Spec: ["{\\operatorname{Spec}\\left(#1\\right)}", 1], // Spectrum of a ring
+            Specmax: ["{\\operatorname{Spec}_{max}\\left(#1\\right)}", 1], // Maximal spectrum of a ring
+            Specrab: ["{\\operatorname{Spec}_{rab}\\left(#1\\right)}", 1], // Rabinowitsch spectrum of a ring
+            N: "\\mathbb{N}", // Natural numbers
+            I: "\\mathcal{I}", // Vanishing ideal
+            V: "{\\mathcal{V}}", // Affine variety
+            H: "{\\mathcal H}", // Hausdorff measure
+            Hauseps: ["{\\H_\\varepsilon^{#1}}", 1], // Hausdorff measure approximation
+            contradiction: "{\\ \\Rightarrow\\!\\Leftarrow}",
+            tab: "{\\ \\ \\ \\ }"
+        },
+        processEscapes: true,
+        processEnvironments: true
+    },
+    options: {
+        ignoreHtmlClass: ".*|",
+        processHtmlClass: "arithmatex"
+    }
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise()
+})
